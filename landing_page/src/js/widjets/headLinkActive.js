a42ae368276198aacc455c80e4c4c41e748ba42d
volitilov <volitilov@gmail.com>
2017-07-19 22:22:23 +0300

    add header active link for scroll window

--- landing_page/src/js/widjets/headLinkActive.js
+++ landing_page/src/js/widjets/headLinkActive.js
@@ -1,13 +1,54 @@
 var j = jQuery.noConflict();
 
 j(function() {
-	j('.navbar-right a').click(function() {
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	alert( "Текущая прокрутка: " + scrollTop );
 
+	function clearActive() {
 		j('.navbar-right a').each(function() {
 			if (j(this).hasClass('active')) {
 				j(this).removeClass('active');
 			}
 		});
+	}
+
+	function addClassActive(link) {
+		j('.navbar-right a').each(function() {
+			if (j(this).text() == link) {
+				j(this).addClass('active');
+			}
+		});
+	}
+
+	j(window).scroll(function(){
+		var scrTop = j("body").scrollTop();
+
+		if( scrTop >= 0) {
+			clearActive();
+			addClassActive('Home');
+		}
+
+		if ( scrTop >= 774 ) {
+			clearActive();
+			addClassActive('About');
+		}
+
+		if ( scrTop >= 2938 ) {
+			clearActive();
+			addClassActive('Portfolio');
+		}
+
+		if (  scrTop >= 4081 ) {
+			clearActive();
+			addClassActive('Services');
+		}
+
+	})
+
+
+	j('.navbar-right a').click(function() {
+
+		clearActive();
 
 		j(this).addClass('active');
 		var el = j(this).attr('href');
@@ -15,4 +56,8 @@ j(function() {
 		    scrollTop: j(el).offset().top}, 1800);
 		return false;
 	});
+
+
+
+
 });
