d4542b564c482e676f6063b3a622125d91372660
volitilov <volitilov@gmail.com>
2017-07-24 05:43:01 +0300

    fix: popup control

--- landing_page/demo/js/widjets/popup.js
+++ landing_page/demo/js/widjets/popup.js
@@ -3,40 +3,58 @@ var j = jQuery.noConflict();
 j(function() {
 	let count = 0;
 	// copy portfolio item and append in popup
-	j('.portfolio-item').each(function() {
-		count += 1;
+	function addElementsInPopup(objects) {
+		j(objects).each(function() {
+			count += 1;
 
-		var popupItem = document.createElement('li');
-		var itemImg = j(this).find('img').clone();
+			let popupItem = document.createElement('li');
+			let itemImg = j(this).find('img').clone();
 
-		j(this).find('a').attr('data-portfolio-item', count);
-		j(popupItem).attr('data-item', count);
+			j(this).find('a').attr('data-portfolio-item', count);
+			j(popupItem).attr('data-item', count);
 
-		j(popupItem).addClass('item');
-		j(popupItem).addClass('animated');
-		j(popupItem).addClass('fadeIn');
-		j(popupItem).append(itemImg);
-		
-		j('.popup-carousel').append(popupItem);
-	});
-	
+			j(popupItem).addClass('item');
+			j(popupItem).addClass('animated');
+			j(popupItem).addClass('fadeIn');
+			j(popupItem).append(itemImg);
+			
+			j('.popup-carousel').append(popupItem);
+		});
+	}
+
+	addElementsInPopup('.portfolio-items2 li');
 
 	// show active portfolio in popup
 	j('.portfolio-item a').click(function(event) {
 		var event = event || window.event;
 		event.preventDefault();
 
-		var portf_item = j(this).attr('data-portfolio-item');
+		let portf_item = j(this).attr('data-portfolio-item');
+		let item_active;
+		let carma = 0;
 
 		j('.popup-carousel li').each(function() {
+			carma += 1;
 			if (j(this).hasClass('active')) {
 				j(this).removeClass('active');
 			}
 
 			if (j(this).attr('data-item') == portf_item) {
 				j(this).addClass('active');
+				item_active = j(this).attr('data-item');
 			}
 		});
+
+		switch (item_active) {
+			case 1:
+				j('.popup-left').hide();
+				break;
+			case carma:
+				j('.popup-right').hide();
+				break;
+			default:
+				j('.popup-left, .popup-right').show();
+		}
 
 		j(".popup-window").show();
 
@@ -63,10 +81,10 @@ j(function() {
 		var count_item = 0;
 
 		j('.popup-carousel li').each(function() {
-			count_item++;
+			count_item += 1;
 			if (j(this).hasClass('active')) {
-				j(this).removeClass('active');
 				active_item = j(this).attr('data-item');
+				j(this).removeClass('active');
 			}
 		});
 
